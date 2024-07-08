5e1b51e2be071a66c006aefd35b5b406b6d273f8
Михаил Дорошин <demargorn@gmail.com>
2024-07-08 15:26:17 +0300

    Оптимизирован код

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,42 +1,16 @@
 'use strict';
 
 const rotatorCases = Array.from(document.querySelectorAll('.rotator__case')); // массив текстовых элементов
-let idx = rotatorCases.findIndex((rotatorCase) => rotatorCase.classList.contains('rotator__case_active')); // индекс активного элемента
+let delay = 1000;
+let count = 0;
 
-function nextRotatorCase() {
-   rotatorCases[idx].classList.remove('rotator__case_active');
-
-   if (idx === rotatorCases.length - 1) {
-      idx = 0;
-   } else {
-      idx += 1;
-   }
-   rotatorCases[idx].classList.add('rotator__case_active');
-
-   switch (rotatorCases[idx].dataset.color) {
-      case 'red':
-         rotatorCases[idx].style.color = 'red';
-         setTimeout(nextRotatorCase, rotatorCases[idx].dataset.speed);
-         break;
-      case 'green':
-         rotatorCases[idx].style.color = 'green';
-         setTimeout(nextRotatorCase, rotatorCases[idx].dataset.speed);
-         break;
-      case 'blue':
-         rotatorCases[idx].style.color = 'blue';
-         setTimeout(nextRotatorCase, rotatorCases[idx].dataset.speed);
-         break;
-      case 'gray':
-         rotatorCases[idx].style.color = 'gray';
-         setTimeout(nextRotatorCase, rotatorCases[idx].dataset.speed);
-         break;
-      default:
-         rotatorCases[idx].style.color = '#000';
-   }
+function toggleClass() {
+   rotatorCases.forEach((element) => element.classList.remove('rotator__case_active'));
+   rotatorCases[count].classList.add('rotator__case_active');
+   count = rotatorCases[count].nextElementSibling ? count + 1 : 0;
+   delay = rotatorCases[count].getAttribute('data-speed');
+   let color = rotatorCases[count].getAttribute('data-color');
+   rotatorCases[count].setAttribute('style', `color: ${color}`);
+   setTimeout(toggleClass, delay);
 }
-
-function rotator() {
-   setInterval(nextRotatorCase, 3500);
-}
-
-rotator();
+setTimeout(toggleClass, delay);
