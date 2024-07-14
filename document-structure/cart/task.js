e9f34c06ed75a2103118dc950ad1a0facee03a56
Михаил Дорошин <demargorn@gmail.com>
2024-07-14 19:00:23 +0300

    Задание выполнено

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -9,32 +9,38 @@ let productCount = 1;
 let productImg = '';
 
 minusBtns.forEach((minusBtn) => {
-   minusBtn.addEventListener('click', (e) => {
+   minusBtn.addEventListener('click', () => {
       if (productCount > 1) {
          productCount = --minusBtn.closest('.product__quantity-controls').querySelector('.product__quantity-value').textContent;
       }
-      productImg = minusBtn.closest('.product').querySelector('img').src;
-      productId = minusBtn.closest('.product').dataset.id;
    });
 });
 
 plusBtns.forEach((plusBtn) => {
-   plusBtn.addEventListener('click', (e) => {
+   plusBtn.addEventListener('click', () => {
       productCount = ++plusBtn.closest('.product__quantity-controls').querySelector('.product__quantity-value').textContent;
-      productImg = plusBtn.closest('.product').querySelector('img').src;
-      productId = plusBtn.closest('.product').dataset.id;
    });
 });
 
 addButtons.forEach((addButton) => {
    addButton.addEventListener('click', (e) => {
+      productImg = e.target.closest('.product').querySelector('img').src;
+      productId = e.target.closest('.product').dataset.id;
       let product = `<div class="cart__product" data-id=${productId}>
                         <img class="cart__product-image" src=${productImg}>
                         <div class="cart__product-count">${productCount}</div>
                      </div>`;
-      basket.insertAdjacentHTML('beforeend', product);
+
+      setTimeout(() => {
+         const cardsInBasket = [...basket.querySelectorAll('.cart__product')]; // товары в корзине
+
+         for (let i = 0; i < cardsInBasket.length; i += 1) {
+            if (productId === cardsInBasket[i].dataset.id) {
+               cardsInBasket[i].querySelector('.cart__product-count').textContent = productCount;
+               return;
+            }
+         }
+         basket.insertAdjacentHTML('beforeend', product);
+      }, 200);
    });
 });
-
-// TODO:
-// Если товар уже имеется в корзине, количество необходимо увеличить, не добавляя в корзину новый элемент.
