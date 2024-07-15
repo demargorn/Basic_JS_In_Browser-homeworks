a35167646f4b5b5a540a6b8a07fe3c1101b51690
Михаил Дорошин <demargorn@gmail.com>
2024-07-15 18:09:37 +0300

    Внесены изменения в слушатель по добавлению в корзину

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -4,9 +4,7 @@ const minusBtns = [...document.querySelectorAll('.product__quantity-control_dec'
 const plusBtns = [...document.querySelectorAll('.product__quantity-control_inc')]; // массив кнопок увеличения
 const addButtons = [...document.querySelectorAll('.product__add')]; // кнопки добавить в корзину
 const basket = document.querySelector('.cart__products'); // корзина
-let productId = 0;
-let productCount = 1;
-let productImg = '';
+let productCount = [...document.querySelectorAll('.product__quantity-value')].textContent; // поля кол-ва товара
 
 minusBtns.forEach((minusBtn) => {
    minusBtn.addEventListener('click', () => {
@@ -24,23 +22,21 @@ plusBtns.forEach((plusBtn) => {
 
 addButtons.forEach((addButton) => {
    addButton.addEventListener('click', (e) => {
-      productImg = e.target.closest('.product').querySelector('img').src;
-      productId = e.target.closest('.product').dataset.id;
-      let product = `<div class="cart__product" data-id=${productId}>
+      const productImg = e.target.closest('.product').querySelector('img').src;
+      const productId = e.target.closest('.product').dataset.id;
+      const product = `<div class="cart__product" data-id=${productId}>
                         <img class="cart__product-image" src=${productImg}>
-                        <div class="cart__product-count">${productCount}</div>
+                        <div class="cart__product-count">
+                           ${e.target.closest('.product__controls').querySelector('.product__quantity-value').textContent}
+                        </div>
                      </div>`;
 
-      setTimeout(() => {
-         const cardsInBasket = [...basket.querySelectorAll('.cart__product')]; // товары в корзине
+      const inBasket = [...basket.querySelectorAll('.cart__product')].find((card) => card.dataset.id === productId); // находим товар в корзине
 
-         for (let i = 0; i < cardsInBasket.length; i += 1) {
-            if (productId === cardsInBasket[i].dataset.id) {
-               cardsInBasket[i].querySelector('.cart__product-count').textContent = productCount;
-               return;
-            }
-         }
+      if (inBasket) {
+         inBasket.querySelector('.cart__product-count').textContent = productCount;
+      } else {
          basket.insertAdjacentHTML('beforeend', product);
-      }, 200);
+      }
    });
 });
