f19ba13560f734aa44dcf78fda3f93d085b3105f
Михаил Дорошин <demargorn@gmail.com>
2024-07-17 20:05:55 +0300

    Исправлено: добавление подсказки только по нажатию на ссылку

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,34 +1,28 @@
 'use strict';
 
 const hasTooltips = Array.from(document.querySelectorAll('.has-tooltip')); // слова с подсказками
+const div = document.createElement('div'); // создаем элемент подсказки
+div.classList.add('tooltip');
 
 hasTooltips.forEach((hasTooltip) => {
    const text = hasTooltip.title; // текст подсказки
    const positionLeft = hasTooltip.getBoundingClientRect().left; // левая гранца нашего слова
    const positionTop = hasTooltip.getBoundingClientRect().bottom; // нижняя граница нашего слова
 
-   const div = document.createElement('div'); // создаем элемент подсказки
-   div.classList.add('tooltip');
-   div.textContent = text;
-
    hasTooltip.addEventListener('click', (e) => {
       e.preventDefault();
 
+      div.textContent = text;
       div.style.left = positionLeft + 'px';
       div.style.top = positionTop + 'px';
       hasTooltip.insertAdjacentElement('afterEnd', div);
 
       if (e.target.title === div.textContent) {
-         div.classList.toggle('tooltip_active');
+         div.classList.add('tooltip_active');
          return;
       } else {
          div.textContent = text;
          div.classList.add('tooltip_active');
       }
-
-      console.log(e.target.title);
    });
-   // hasTooltip.addEventListener('blur', () => {
-   //    div.remove();
-   // });
 });
