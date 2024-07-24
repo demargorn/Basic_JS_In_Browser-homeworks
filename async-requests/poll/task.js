78aec6c7e24f886f117c227fbc2c2d7fff60fdee
Михаил Дорошин <demargorn@gmail.com>
2024-07-24 16:22:40 +0300

    Добавлен вариант с fetch

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -75,3 +75,66 @@ class Poll {
    }
 }
 const poll = new Poll('.poll');
+
+// with fetch
+const title = document.getElementById('poll__title');
+const pool = document.getElementById('poll__answers');
+const url = 'https://students.netoservices.ru/nestjs-backend/poll';
+
+fetch(url)
+   .then((response) => response.json())
+   .then((data) => {
+      title.innerText = data.data.title;
+
+      data.data.answers.forEach((item, index) => {
+         const btn = document.createElement('button');
+
+         btn.classList.add('poll__answer');
+         btn.style.marginRight = '5px';
+         btn.innerText = item;
+
+         pool.insertAdjacentElement('afterbegin', btn);
+
+         btn.addEventListener('click', (e) => {
+            alert('Спасибо, ваш голос засчитан!');
+            fetch(url, {
+               method: 'POST',
+               headers: {
+                  'Content-Type': 'application/x-www-form-urlencoded',
+               },
+               body: `vote=${data.id}&answer=${index}`,
+            })
+               .then((response) => response.json())
+               .then((data) => {
+                  document.body.innerHTML = '';
+                  const allVotes = data.stat.reduce((acc, val) => {
+                     return acc + Number(val.votes);
+                  }, 0);
+
+                  const result = document.createElement('div');
+                  result.style.margin = '10px 0 0 10px';
+                  const next = document.createElement('button');
+                  next.setAttribute('type', 'button');
+                  next.innerText = 'Дальше';
+                  next.addEventListener('click', () => {
+                     location.reload();
+                  });
+
+                  document.body.appendChild(result);
+                  data.stat.forEach((item) => {
+                     const title = document.createElement('div');
+                     title.style.marginBottom = '10px';
+                     const percentage = document.createElement('span');
+                     title.innerText = item.answer;
+                     percentage.innerText = ': ' + parseInt((item.votes / allVotes) * 100) + '%';
+                     percentage.style.fontWeight = 'bold';
+                     title.appendChild(percentage);
+                     result.appendChild(title);
+                  });
+                  result.appendChild(next);
+               })
+               .catch((error) => alert(error));
+         });
+      });
+   })
+   .catch((error) => alert(error));
